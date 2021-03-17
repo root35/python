97f187abd5664f313afad5aff5a363f4d71b403e
root35 <rakho.myriam@gmail.com>
2021-03-17 12:44:06 +0100

    updates

---
+++ py-07-realpython-advanced-tutorials/test_main_functions.py
@@ -0,0 +1,3 @@
+print("This is a file to test Python's execution modes")
+print("The __name__ variable tells the context in which this file is running")
+print(f"The value of __name__ is {repr(__name__)}")
