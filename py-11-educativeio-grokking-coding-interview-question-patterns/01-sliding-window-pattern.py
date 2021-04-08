4d15c1ada277e0ecf1b2accfea4a943f0d201134
root35 <rakho.myriam@gmail.com>
2021-04-08 12:09:03 +0200

    update

--- py-11-educativeio-grokking-coding-interview-question-patterns/01-sliding-window-pattern.py
+++ py-11-educativeio-grokking-coding-interview-question-patterns/01-sliding-window-pattern.py
@@ -1,4 +1,24 @@
-## EASY
+def find_averages_of_subarrays(K, arr):
+  result = []
+  for i in range(len(arr) - K + 1):
+    # find sum of next 'K' elements
+    _sum = 0.0
+    for j in range(i, i + K):
+      _sum += arr[j]
+    result.append(_sum / K)  # calculate average
+
+  return result
+
+
+def main():
+  result = find_averages_of_subarrays(5, [1, 3, 2, 6, -1, 4, 1, 8, 2])
+  print("Averages of subarrays of size K: " + str(result))
+
+
+main()
+
+
+# EASY
 #  Maximum sum subarray of size K
 #  https://www.educative.io/courses/grokking-the-coding-interview/JPKr0kqLGNP
 # time O(n) - space O(1)
@@ -19,18 +39,15 @@ def max_sub_array_of_size_k(k, arr):
   return max_win_sum
 
 
-
-
-
-
-
-
-## EASY
+# EASY
 #  Smallest subarray with given sum
 #  https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
 # time O(n) - space O(1)
 
+
 import math
+
+
 def smallest_subarray_with_given_sum(s, arr):
   print('')
   win_sum = 0.0
@@ -50,18 +67,14 @@ def smallest_subarray_with_given_sum(s, arr):
   return min_win_len
 
 
-
-
-
-
-
-
-## MEDIUM
+# MEDIUM
 #  Longest substring with k distinct characters
 #  https://www.educative.io/courses/grokking-the-coding-interview/YQQwQMWLx80
 # Runtime O(n) - Space O(k) we store max k+1 characters
 
+
 from collections import defaultdict
+
 
 def longest_substring_with_k_distinct(str, k):
   char_freqs = defaultdict(int)
@@ -82,19 +95,13 @@ def longest_substring_with_k_distinct(str, k):
 
   return max_len
 
-
-
-
-
-
-
-
-  ## MEDIUM
+  # MEDIUM
   #  Fruits into baskets
   #  https://www.educative.io/courses/grokking-the-coding-interview/Bn2KLlOR0lQ
   # time O(n) - space O(1)
 
   from collections import defaultdict
+
 
 def fruits_into_baskets(fruits):
   n_baskets = 2
@@ -118,19 +125,15 @@ def fruits_into_baskets(fruits):
   return -1
 
 
-
-
-
-
-
-
-## MEDIUM
+# MEDIUM
 #  Longest substring with no repeat characters
 #  https://www.educative.io/courses/grokking-the-coding-interview/YMzBx1gE5EO
 # time O(n) - space O(k) k number of distinct chars in input string
 
+
 from collections import defaultdict
 from functools import reduce
+
 
 def non_repeat_substring(str):
   start, max_len = 0, 0
@@ -154,19 +157,15 @@ def non_repeat_substring(str):
   return max_len
 
 
-
-
-
-
-
-
-## HARD
+# HARD
 #  Pattern permutation in a string
 #  https://www.educative.io/courses/grokking-the-coding-interview/N8vB7OVYo2D
 # - time O(n + m) len of str and pattern resp.
 # - space O(m)    chars in pattern in dict
 
+
 from collections import defaultdict
+
 
 def find_permutation(str1, pattern):
   start, matched = 0, 0
@@ -193,7 +192,7 @@ def find_permutation(str1, pattern):
       if left_char in pattern_char_freqs:
         if pattern_char_freqs[left_char] == 0:
           matched -= 1
-        pattern_char_freqs[left_char] += 1 # not matched anymore
+        pattern_char_freqs[left_char] += 1  # not matched anymore
 
   return False
 
