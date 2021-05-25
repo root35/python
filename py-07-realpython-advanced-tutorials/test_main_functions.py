e105ef5aa1dc4ac15d2ca1bec63c983380c7b99a
root35 <rakho.myriam@gmail.com>
2021-05-25 19:54:12 +0200

    Remove py-07... folder

--- py-07-realpython-advanced-tutorials/test_main_functions.py
+++
@@ -1,3 +0,0 @@
-print("This is a file to test Python's execution modes")
-print("The __name__ variable tells the context in which this file is running")
-print(f"The value of __name__ is {repr(__name__)}")
