a49ff0c5c84a077e955bb7c19c2b1236f566f7b5
root35 <rakho.myriam@gmail.com>
2021-05-25 19:26:47 +0200

    Remove py-03-powerfulpython-3keys

--- py-03-powerfulpython-3keys/Courseware-3KEYS/labs/generators-squares.py
+++
@@ -1,8 +0,0 @@
-def gen_squares(max_root):
-    for root in range(max_root):
-        yield root**2
-
-
-squares = gen_squares(5)
-for square in squares:
-    print(square)
--- py-03-powerfulpython-3keys/Courseware-3KEYS/labs/test_greeting.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from greeting import greet
-
-
-class TestGreeting(unittest.TestCase):
-    def test_greet(self):
-        self.assertEqual('Hi, John', greet('John'))
