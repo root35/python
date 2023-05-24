280db0c49e23f73bb862fe3e7765f6fcedd7d846
Myriam RAKHO <rakho.myriam@gmail.com>
2023-05-24 19:43:30 +0200

    Update howto-jupyter.sh

--- 00-howtos/howto-jupyter.sh
+++ 00-howtos/howto-jupyter.sh
@@ -1,3 +1,8 @@
 #### Set Jupyter password
 
 https://jupyter-notebook.readthedocs.io/en/stable/public_server.html
+
+#### Launch notebook in Chrome
+- Set Chrome as default browser in the Chrome settings
+- Open terminal and type
+$ jupyter notebook
