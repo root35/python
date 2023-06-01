f4348bcb9cdeb1a62a21c67b16535328a4f7beaa
Myriam RAKHO <rakho.myriam@gmail.com>
2023-06-01 17:53:27 +0200

    Update howto-jupyter.sh

--- 00-howtos/howto-jupyter.sh
+++ 00-howtos/howto-jupyter.sh
@@ -1,8 +1,30 @@
 #### Set Jupyter password
-
 https://jupyter-notebook.readthedocs.io/en/stable/public_server.html
+
 
 #### Launch notebook in Chrome
 - Set Chrome as default browser in the Chrome settings
 - Open terminal and type
 $ jupyter notebook
+
+
+#### Add environment to Jupyter Notebook with specific Python version
+# Ref:
+# https://sites.northwestern.edu/researchcomputing/resources/adding-python-3-to-jupyter-notebook/
+
+$ conda create -n py39 python=3.9 ipykernel jupyter anaconda
+$ source activate py39
+(OR: $ conda activate py39)
+$ ipython kernel install --name py39 --user
+# Close terminal and open a new one
+
+# https://conda.io/projects/conda/en/latest/user-guide/tasks/manage-environments.html#setting-environment-variables
+$ conda env config vars set OPENAI_API_KEY=...
+$ conda activate py39  # restart env
+$ echo $OPENAI_API_KEY
+
+$ (base) macbook nltk_data >> jupyter notebook   # -> py39
+$ (base) macbook ~ >> jupyter notebook           # -> default
+
+
+
